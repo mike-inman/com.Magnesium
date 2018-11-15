b60e1461c10903528baa0749e462ff45df24903c
Michael Inman <michael.d.inman@gmail.com>
2018-11-15 09:33:12 -0600

    Add project files.

---
+++ com.Magnesium/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace com.Magnesium.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
